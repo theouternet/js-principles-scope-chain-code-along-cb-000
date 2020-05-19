23dc98c798fb92f910b818273638a4ac687c54e6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-19 21:35:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 /* Your code goes here */
+
+customerName = 'bob';
+const leastFavoriteCustomer = 'tom'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'tim';
+}
+
+function attemptTwoFavoriteCustomers () {
+ let favoriteCustomer = 'blake';
+
+  throw new SyntaxError('err', 'blah.js', 1);
+
+
+}
